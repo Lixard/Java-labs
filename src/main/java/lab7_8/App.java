6433c93161c7e2eb74e7fb974aee839fa9cfe6f6
Lixard <51443500+Lixard@users.noreply.github.com>
2019-11-15 01:59:40 +0300

    created db design and sql scripts, moved to postgresql

--- src/main/java/lab7_8/App.java
+++ src/main/java/lab7_8/App.java
@@ -9,9 +9,9 @@ import java.sql.Statement;
 
 public final class App {
 
-    private static final String URL = "jdbc:mysql://127.0.0.1:3306/lab78?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "12345";
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/vus";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "123";
     private static final Flyway flyway = Flyway.configure().dataSource(URL, USERNAME, PASSWORD).load();
 
     public static void main(String[] args) {
---
+++ src/main/resources/db/migration/V1.2__createTables.sql
@@ -0,0 +1,39 @@
+CREATE TABLE "vuss" (
+	"id" serial NOT NULL,
+	"name" varchar(50),
+	"adress" varchar(100),
+	CONSTRAINT "vuss_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "sotr" (
+	"id" serial NOT NULL,
+	"idvuss" integer,
+	"name" varchar(50),
+	"fone" varchar(10),
+	CONSTRAINT "sotr_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+CREATE TABLE "spec" (
+	"id" serial NOT NULL,
+	"idvuss" integer,
+	"name" varchar(50),
+	"info" varchar(100),
+	CONSTRAINT "spec_pk" PRIMARY KEY ("id")
+) WITH (
+  OIDS=FALSE
+);
+
+
+
+
+ALTER TABLE "sotr" ADD CONSTRAINT "sotr_fk0" FOREIGN KEY ("idvuss") REFERENCES "vuss"("id");
+
+ALTER TABLE "spec" ADD CONSTRAINT "spec_fk0" FOREIGN KEY ("idvuss") REFERENCES "vuss"("id");
